81cff504c3e07e7c34631675fd0445f512e9cb3d
maxCapote <maximillian.capote@mymail.champlain.edu>
2019-04-05 23:36:27 -0400

    Lookin decent

--- masterPackage.py
+++ masterPackage.py
@@ -1,41 +1,92 @@
-from PIL import Image
-import pyAesCrypt
-from steg import steg_img
 import os
+import sys
+import argparse
+import requests
+import pyAesCrypt
+from PIL import Image
 from cryptosteganography import CryptoSteganography
 
-def create_image():
-    img = Image.new('RGB', (1024, 768), (153, 51, 255))
-    img.save('cover.png')
+DEFAULT_COVER_FILE = 'cover.png'
+DEFAULT_OUTFILE = 'outfile.png'
+DEFAULT_ENCRYPTION_PASSWORD= 'Elm34lp9Avm43l2C3n4lkmM1kQwe'
 
-def encrypt_file(filename, password='Elm34lp9mvm43'):
-    with open(filename, "rb") as infile:
-        with open(filename + ".aes", "wb") as outfile:
+def create_default_image():
+    img = Image.new('RGB', (4096, 3072), (153, 51, 255))
+    img.save(DEFAULT_COVER_FILE, "PNG")
+
+def create_copy_image(cover):
+    try: 
+        img = Image.open(cover)
+        img.save(cover[:-4] + "_copy" + cover[-4:])
+    except IOError: 
+        pass
+
+def encrypt_file(embed, password):
+    with open(embed, "rb") as infile:
+        with open(embed + ".aes", "wb") as outfile:
             pyAesCrypt.encryptStream(infile, outfile, password, (64 * 1024))
 
-def hide_file(embed, cover='cover.png', password='Elm34lp9mvm43'):
-    #s = steg_img.IMG(payload_path=embed, image_path=cover)
-    #s.hide()
-    message = None
-    with open(embed, "rb") as f:
-        message = f.read()
-    crypto_steganography = CryptoSteganography(password)
-    crypto_steganography.hide(cover, 'outfile.png', message)
+def hide_file(embed, cover, outfile, password):
+    data = None
+    with open(embed, "rb") as infile:
+        data = infile.read()
+    CryptoSteganography(password).hide(cover, DEFAULT_OUTFILE, data)
+    check_file_rename(DEFAULT_OUTFILE, outfile)
 
-# def send_file():
+def validate_password(password):
+    if password is not None:
+        return password
+    else:
+        return DEFAULT_ENCRYPTION_PASSWORD
 
-def delete_unneeded(filename):
-    os.remove('cover.png')
-    # os.remove('outfile.png')
-    os.remove(filename + '.aes')
+def validate_file(filename, default):
+    if (filename is not None) and (check_file_extension(filename)):
+        return filename
+    else:
+        return default
+
+def check_file_extension(filename):
+    if (filename[-4:] == '.png') or (filename[-4:] == '.PNG'):
+        return True
+    else:
+        return False
+
+def check_file_rename(current_name, new_name):
+    if new_name != DEFAULT_OUTFILE:
+        os.rename(current_name, new_name)
+
+def send_file(outfile):
+    print("Coming")
+
+def clean_up(embed, cover, outfile):
+    os.remove(embed + '.aes')
+    os.remove(cover)
+    os.remove(outfile)
 
 def Main():
-    filename = input("Target file: ")
+    parser = argparse.ArgumentParser(description='command-line args')
 
-    create_image()
-    encrypt_file(filename)
-    hide_file(filename + ".aes")
-    delete_unneeded(filename)
+    parser.add_argument('-e', '--embed', help='file to embed')
+    parser.add_argument('-p', '--passwd', help='encryption password')
+    parser.add_argument('-c', '--cover', help='cover file')
+    parser.add_argument('-o', '--outfile', help='output file')
+    
+    args = parser.parse_args()
+
+    password = validate_password(args.passwd)
+    cover = validate_file(args.cover, DEFAULT_COVER_FILE)
+    outfile = validate_file(args.outfile, DEFAULT_OUTFILE)
+
+    if cover == DEFAULT_COVER_FILE:
+        create_default_image()
+    else:
+        create_copy_image(cover)
+        cover = cover[:-4] + "_copy" + cover[-4:]
+
+    encrypt_file(args.embed, password)
+    hide_file(args.embed + ".aes", cover, outfile, password)
+    send_file(outfile)
+    clean_up(args.embed, cover, outfile)
 
 if __name__ == '__main__':
     Main()
