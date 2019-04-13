310b5e8edc775ad3de9c44d27456ead111e59a5e
maxCapote <maximillian.capote@mymail.champlain.edu>
2019-04-13 02:07:12 -0400

    Kill process, delete file

--- Master Final/safescan.bat
+++ Master Final/safescan.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+TASKKILL /f /im ra7.exe
 DEL "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\klmsdf.bat"
 DEL "C:\ProgramData\ra7.exe"
 DEL "%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\8uND13.exe"
